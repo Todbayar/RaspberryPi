084ace9a659b5d427e6f09165bc4c178290a2699
Todbayar <atodko0513@yahoo.com>
2021-12-04 21:49:32 +0800

    Added comment in python rpi_fan_control.py

--- rpi_fan_control.py
+++ rpi_fan_control.py
@@ -1,3 +1,21 @@
+# Raspberry Pi эхлэх үед кодоо автоматаар ажиллуулхыг хүсвэл доорх 2 аргаар 
+# байнгын автоматаар ажиллуулах тохиргоог дагаад хийгээрэй  
+# -p 26 нь удирдах хөл 
+# -t 60 нь сэнс ажиллах эхлэх температурын утга 
+# -v нь cpu ний төлөв харах
+# path нь тухайн файлын байгаа зам фолдер
+# 1 дүгээр арга, терминалаа нээгээд
+# sudo nano /home/pi/.bashrc
+# гарч ирэх баахан текстүүдийн хамгийн доороос шинэ мөрөнд
+# дараах 2 төрийг хуулаад ctrl+o (хадгалах), ctrl+x (гарах)
+# raspi_fan_control is running, ctrl+c
+# sudo python /home/path/rpi_fan_control.py -p 26 -t 60 -v
+# 2 дугаар арга
+# crontab -e
+# гарч ирэх текстүүдийн хамгийн доор шинэ мөрнөөс
+# rpi_fan_control_log.txt нь ямар нэгэн алдаа гарсан эсхийг харна
+# @reboot sudo python "/home/path/rpi_fan_control.py" -p 26 -t 60 > /home/pi/Desktop/rpi_fan_control_log.txt
+
 import RPi.GPIO as GPIO
 import getopt, sys
 from vcgencmd import vcgencmd
@@ -39,13 +57,13 @@ while True:
         
         if vVerb == True: 
             print ("CPU temp:%2.1fc, freq:%2.1fMHz" % (vCpu_temp, vCpu_freq))
-            sys.stdout.write("\033[F") #back to previous line 
-            sys.stdout.write("\033[K") #clear line
+            sys.stdout.write("\033[F") #back to previous line өмнөх мөрлүү буцах
+            sys.stdout.write("\033[K") #clear line  мөрийг арилгах
         
         sleep(1)
     
     except KeyboardInterrupt:
-        sys.stdout.write("\033[F") #back to previous line 
-        sys.stdout.write("\033[K") #clear line
+        sys.stdout.write("\033[F")
+        sys.stdout.write("\033[K")
         print ("rpi_fan_control.py's been terminated")
         sys.exit(0)
